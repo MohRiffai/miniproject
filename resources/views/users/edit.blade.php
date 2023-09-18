4cc4a5c030c1fbcca5fa61a8f4419673248223c8
MohRiffai <morifa007@gmail.com>
2023-09-18 11:58:44 +0700

    penyesuaian keterangan pada ganti password form edit user

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -27,7 +27,7 @@
                 <label for="password" class="col-sm-2 col-form-label">Password</label>
                 <div class="col-sm-10">
                     <input type="password" class="form-control" name="password" placeholder="Enter new password" id="password">
-                    <p>kosongkan bial password tidak diganti</p>
+                    <p style="color:red; font-size:14px; font-style:italic ">(Kosongkan bila password tidak diganti)</p>
                 </div>
             </div>            
             <div class="mb-3 row">
